346f5e6bc492f59c90e2a18482a688fdc15fbfba
SachiniGinige <SachiniGinige@users.noreply.github.com>
2020-07-24 23:19:57 +0530

    Revert "Update zoderForm.php"
    This reverts commit 6852569f0eccae591eebc8106dadc444be6f6f23.

--- zoderForm.php
+++ zoderForm.php
@@ -276,7 +276,6 @@
 			
 			if (pattern.test(val)!=true){
 				msgString="Please enter valid email address."
-                document.getElementById("emailI").style.display = 'none';
 			}
 			else{
                 document.getElementById("emailI").style.display = 'inline';
